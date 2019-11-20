062ff94b472c744212f62dc3ade68a76f7868824
Kah919 <kahyap13@gmail.com>
2019-11-20 03:16:50 -0500

    create name database

---
+++ name-rater/src/server.js
@@ -0,0 +1,20 @@
+const Data = require('./data.js')
+
+const express = require('express');
+const app = express();
+
+app.get('/api/names', (req, res) => {
+    res.json(Data);
+});
+
+app.get('/api/names/male', (req, res) => {
+    res.json(Data.male);
+});
+
+app.get('/api/names/female', (req, res) => {
+    res.json(Data.female);
+})
+
+const port = 5000;
+
+app.listen(port, () => console.log(`server started on ${port}`));
