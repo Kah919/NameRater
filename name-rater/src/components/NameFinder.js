e22cdfd555bbf9b9c62c16da52709dc5958e2298
Kah919 <kahyap13@gmail.com>
2019-11-20 21:28:28 -0500

    change NameList from functional component to class component

---
+++ name-rater/src/components/NameFinder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class NameFinder extends Component {
+    state = {
+        sex: "",
+        name: ""
+    }
+
+    render() {
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default NameFinder;
--- name-rater/src/components/NameList.js
+++ name-rater/src/components/NameList.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 
-export default props => {
-
-    let list = () => {
-        fetch(`http://localhost:5000/api/names/male`)
-        .then(res => res.json())
-        .then(data => console.log(data))
+class NameList extends Component {
+    state = {
+        list: []
     }
 
-    return(
-        <>
-            <h1> HI </h1>
-            { list() }
-        </>
-    )
+    componentDidMount() {
+        fetch(`http://localhost:5000/api/names/male`)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                list: data
+            })
+        })
+    }
+
+    render() {
+        console.log(this.state.list)
+        return(
+            <>
+                <h1> HI </h1>
+            </>
+        )
+    }
 }
 
+export default NameList;
