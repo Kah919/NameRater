8101f7bbb9b56edae1f82381c7928fc57a82c796
Kah919 <kahyap13@gmail.com>
2019-12-03 17:24:39 -0500

    seperate server from client

---
+++ name-rater/server/server.js
@@ -0,0 +1,37 @@
+const Data = require('./data.js');
+const express = require('express');
+const app = express();
+require("dotenv").config()
+// const cors = require('cors');
+const mongoose = require('mongoose');
+console.log(process.env.DB_USERNAME)
+// mongoose.connect(`mongodb+srv${process.env.DB_USERNAME}:${process.env.DB_PW}//:@cluster0-ktml6.mongodb.net/test?retryWrites=true&w=majority`);
+// console.log(Data)
+
+// app.use(cors()); 
+
+app.get('/api/names', (req, res) => {
+    res.json(Data);
+    db.User.findAll({})
+    .then( (allUsers) => {
+        res.json(allUsers)
+    })
+});
+
+app.get('/api/names/male', (req, res) => {
+    res.json(Data.male);
+});
+
+app.get('/api/names/female', (req, res) => {
+    res.json(Data.female);
+});
+
+app.get('/api/names/female/:id', (req, res) => {
+    const person = Data.female[parseInt(req.params.id)];
+    if(!person) res.status(404).send('Name not found');
+    res.send(person);
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`server started on ${port}`));
