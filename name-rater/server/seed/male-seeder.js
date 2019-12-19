d9d63b672ce322598c3494b6d0286a8b9fde5b93
Kah919 <kahyap13@gmail.com>
2019-12-19 11:12:29 -0500

    seed male data

--- name-rater/server/seed/male-seeder.js
+++ name-rater/server/seed/male-seeder.js
@@ -1,27 +1,28 @@
 const mongoose = require('mongoose');
 const Male = require('../models/Male');
 const Data = require('../data.js').male;
-let done = 0;
+const db = require('../config/keys').mongoURI;
 
-const seedData = data => {
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+async function seedNames(data) {
     for(let i = 0; i < data.length; i++) {
-        const name = Female({
-            name: data.name,
-            count: data.count
-        });
-
-        name.save((err, result) => {
-            done++;
-            if(done === Data.length) {
-                exit();
-            }
-        });
+        try {
+            const newName = await Male.create({
+                name: data[i].name,
+                count: data[i].count
+            })
+        } catch(e) {
+            console.log(e.message);
+            continue;
+        }
     }
-}
+  }
+  
+  seedNames(Data)
+    .then(() => {
+      mongoose.disconnect();
+    })
 
-const exit = () => {
-    mongoose.disconnect();
-    console.log('exiting')
-}
-
-seedData(Data);
