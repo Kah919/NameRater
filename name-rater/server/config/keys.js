9f5e64e0e04a788e5cbe0aa9c72f13734cdac625
Kah919 <kahyap13@gmail.com>
2019-12-20 13:38:55 -0500

    change user

--- name-rater/server/config/keys.js
+++ name-rater/server/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://kah:kah123@cluster0-424qa.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: `mongodb+srv://kah:namerater123@cluster0-424qa.mongodb.net/test?retryWrites=true&w=majority`
 }
