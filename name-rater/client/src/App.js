731f8e42357337839be9eb30b9aa457f159a4665
Kevin Lai <kevinlai76@gmail.com>
2019-12-23 17:43:47 -0500

    Cors package syntax development, codebase architecture still being decided

--- name-rater/client/src/App.js
+++ name-rater/client/src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 // BrowserRouter is called in the top most component so you can call methods 'Switch', 'Route', 'Link' in the render portion
 import { Switch, Route } from 'react-router-dom'
 import Form from './components/Form';
 import TitleScreen from './components/TitleScreen'
+import Result from './components/Result'
 
-function App() {
+class App extends Component {
+  state = {
+    femaleNames: [],
+    maleNames: []
+  }
+// we have to decide where we want to make the fetch request
+  // componentDidMount() {
+  //   fetch(`http://localhost:5000/api/names/female`)
+  //     .then(res => res.json())
+  //     .then(console.log)
+  // }
+  
 
-  return (
-    // Switch matches the 'path' to the first URL param that comes up;
-    // 'exact' is a key word, it requires  the path to match the URL exact; 
-    // 'component' points the path to render the desired component;
-    <Switch>
-      <Route exact path='/' component={TitleScreen}/>
-      <Route exact path='/search' component={Form}/>
-    </Switch>
-  );
+  render() {
+    return (
+      // Switch matches the 'path' to the first URL param that comes up;
+      // 'exact' is a key word, it requires  the path to match the URL exact; 
+      // 'component' points the path to render the desired component;
+      <Switch>
+        <Route exact path='/' component={TitleScreen}/>
+        <Route exact path='/search' component={Form}/>
+        <Route exact path='/results' component={Result}/>
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- name-rater/server/server.js
+++ name-rater/server/server.js
@@ -1,6 +1,7 @@
 const Data = require('./data.js');
 const express = require('express');
 const app = express();
+// const cors = require('cors')
 const mongoose = require('mongoose');
 const Male = require('./models/Male');
 const Female = require('./models/Female');
@@ -18,6 +19,17 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 // Use routes
 app.use('/api/names', name);
 
+// app.use(cors())
+
+// app.get('/api/names', function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for all origins!'})
+//   })  
+
 const port = 5000;
 
-app.listen(port, () => console.log(`server started on ${port}`));
+app.listen(port, () => {
+    console.log(`server started on ${port}`)
+    // console.log(`CORS-enabled web server listening on ${port}`)
+});
+
+
