0dd48c96ffec79546fe9edd1732ef2660c3eb04c
Kah919 <kahyap13@gmail.com>
2020-01-23 23:16:44 -0500

    create theme

--- name-rater/client/src/components/TitleScreen.js
+++ name-rater/client/src/components/TitleScreen.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Redirect } from 'react-router';
 
-export default () => {
-    return (
-        <div>
-            <h1> Name Rater </h1>
-            <Link to={'/search'}>
-                <button>
-                    Press Start!
-                </button>
-            </Link>
-        </div>
-    )
+
+const Div = styled.div`
+    border: 5px solid black;
+    border-style: double;
+    position: absolute;
+    bottom: 0;
+    height: 35%;
+    width: 100%;
+    border-radius: 10px;
+    font-size: 4rem;
+    padding: 2%;
+    font-family: 'pokemon-font', monospace;
+`;
+
+class TitleScreen extends Component {
+    state = {
+        redirect: false
+    }
+
+    redirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+
+    isRedirect = () => {
+        if(this.state.redirect) {
+            return <Redirect to="/search" />
+        }
+    }
+
+    render() {
+        return (
+            this.state.redirect ? this.isRedirect() : 
+            <Div onClick={ this.redirect }>
+                
+            </Div>
+        )
+    }    
 }
+
+export default TitleScreen;
