56af209d31a92323f779c29d2b97f9b45f7a13a2
Kah919 <kahyap13@gmail.com>
2020-02-20 18:54:26 -0500

    refactor name and result functionality

--- name-rater/client/src/components/NameForm.js
+++ name-rater/client/src/components/NameForm.js
@@ -8,15 +8,16 @@ class NameForm extends Component {
                 <h1>What's Your Name?</h1>
                 <form onSubmit={this.props.handleSubmit}>
                     <input type='text' name='name' value={ this.props.value } onChange={ this.props.handleChange }/>
-                        <Link to={{
+                        {/* <Link to={{
                             pathname: './results',
                             formProps:{
                                 name: this.props.value,
                                 sex: this.props.sex
                             }
                         }}>
+                        
+                        </Link> */}
                         <input type='submit'/>
-                        </Link>
                 </form>
                 </div>
         )
--- name-rater/client/src/containers/MainContainer.js
+++ name-rater/client/src/containers/MainContainer.js
@@ -1,32 +1,77 @@
 import React, { Component } from 'react';
-import Intro from './Intro'
+import Intro from './Intro';
+import NameForm from '../components/NameForm';
+import Result from '../components/Result';
 
 class MainContainer extends Component{
     
     state = {
         gender: "",
-        name: ""
+        name: "",
+        submit: false
     }
 
     handleGenderSelect = (event) => {
-        debugger
         this.setState({
             gender: event.target.dataset.gender
         })
     }
 
+    handleChange = (event) => {
+        let charCode = this.findCharCode(event.target.value)
+        this.backSpace(charCode);
+        this.validInput(charCode, event.target.value);
+    }
+
+    findCharCode = (value) => {
+        if(!this.state.name.length) {
+            return value.charCodeAt()
+        } else if(this.state.name.length >= 1) {
+            return value.charCodeAt(this.state.name.length)
+        }
+    }
+
+    backSpace = charCode => {
+        if(!charCode) {
+            this.setState({
+                name: this.state.name.slice(0, -1)
+            })
+        }
+    }
+
+    validInput = (charCode, value) => {
+        if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+            this.setState({
+                name: value
+            })
+        } else if(charCode){
+            // this.setState({
+            //     modal: true
+            // })
+            alert("WRONG INPUT")
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("submitting", this.state.name)
+        this.setState({submit: true})
+    }
+
     renderComponent = () => {
-        if(this.state.name) {
+        if(this.state.submit) {
             // return result component
-        } else if(this.state.name) {
+            return <Result formProps={ { name: this.state.name, sex: this.state.gender }}/>
+        } else if(this.state.gender) {
             // return name component
+            return <NameForm value={ this.state.name } handleChange={ this.handleChange } handleSubmit={ this.handleSubmit } />
         } else {
-            // return intro component
             return <Intro handleGenderSelect={ this.handleGenderSelect }/>
         }
     }
 
     render(){
+        console.log(this.state)
         return(
             <div>
                 { this.renderComponent() }
